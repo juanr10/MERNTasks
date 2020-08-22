ed653bdb2a11042061c100ee4982c00d63b487db
juan argudo <argudojuan7@gmail.com>
2020-08-22 21:02:48 +0200

    port config updated

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 //App port
-const PORT = process.env.PORT || 4000;
+const port = process.env.port || 4000;
 
 //Import routes
 app.use('/api/users', require('./routes/users'));
@@ -27,6 +27,6 @@ app.use('/api/projects', require('./routes/projects'));
 app.use('/api/tasks', require('./routes/tasks'));
 
 //Run server
-app.listen(PORT, () => {
-    console.log(`The server is running on port ${PORT}`);
+app.listen(port, '0.0.0.0' ,() => {
+    console.log(`The server is running on port ${port}`);
 });
