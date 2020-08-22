a948fd0428bfe1930301684d89d47412dff7ed2c
juan argudo <argudojuan7@gmail.com>
2020-08-22 22:11:04 +0200

    port -> PORT index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use(express.json({ extended: true }));
 
 //App port
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 
 //Import routes
 app.use('/api/users', require('./routes/users'));
